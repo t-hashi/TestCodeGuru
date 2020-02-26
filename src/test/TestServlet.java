705f81efd89f0a00ea0eeadd2b6ba08c463dd547
Toru Hashimoto <g.toru.hashimoto@gmail.com>
2020-02-26 15:41:55 +0900

    test

--- src/test/TestServlet.java
+++ src/test/TestServlet.java
@@ -29,7 +29,7 @@ public class TestServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
-		out.println("Hello World.");
+		out.println("Hello World...");
 	}
 
 	/**
