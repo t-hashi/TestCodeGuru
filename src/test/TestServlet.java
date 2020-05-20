ac2e2a0851fbb3d525517004854a0f024a904a31
't-hashi' <'g.toru.hashimoto@gmail.com'>
2020-05-20 21:49:14 +0900

    test

--- src/test/TestServlet.java
+++ src/test/TestServlet.java
@@ -2,12 +2,19 @@ package test;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+import javax.annotation.Resource;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
 /**
  * Servlet implementation class TestServlet
@@ -15,28 +22,62 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/TestServlet")
 public class TestServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public TestServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	@Resource(name = "jdbc/postgres")
+	private DataSource ds;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public TestServlet() {
+		super();
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
-		out.println("Hello World.......");
+		Connection con = null;
+		long start = 0;
+		long end = 0;       
+
+		try {
+			con = ds.getConnection();
+			//start = System.currentTimeMillis();
+			start = System.nanoTime();
+			PreparedStatement st = (PreparedStatement) con.createStatement();
+			st.setInt(1, 1);
+			//Statement st = (PreparedStatement) con.createStatement();
+			ResultSet rs = st.executeQuery("select id from test2 where id = ?");
+			int recordCount = 0;
+			while (rs.next()) {
+				out.println(rs.getInt("id"));
+				recordCount++;
+			}
+			out.println("<br>");
+			out.print("recordCount:");
+			out.println(recordCount);
+			out.println("<br>");
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			try {
+				con.close();
+				end = System.nanoTime();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		out.print("time:");
+		out.print((end - start)/1000);
+		out.println(" micro sec");
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
